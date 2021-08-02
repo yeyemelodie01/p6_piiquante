bf02ea3e6349c705ba6300126f152177f3f9ace5
melodieyeye <yeyemelodie@outlook.fr>
2021-08-02 16:40:00 +0200

    edit page

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express'); // Importe l'API Express afin que nous puissions utiliser ses fonctionnalités dans notre application.
+
+const app = express(); // Construit une nouvelle application Express qui fonctionne comme notre serveur.
+
+app.use(express.json()); // Indique à l'application Express d'utiliser le middleware JSON (pour que nous puissions voir les corps de nos requêtes en JSON).
+
+app.get('/', (req, res) => { // Crée une route GET et envoie une réponse initiale.
+    res.status(200).json({message: "Hello from my-express-app!"});
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+});
