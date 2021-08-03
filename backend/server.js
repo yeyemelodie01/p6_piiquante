843326c202e3d2d861afeae86e1201d8579ddd25
melodieyeye <yeyemelodie@outlook.fr>
2021-08-03 12:11:15 +0200

    edit page

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-const http = require('http'); //constante http qui donne accès a l'objet http qui permet de creer le serveur
-const app = require('./index');
+require('dotenv').config();
+const express = require("express"); //constante http qui donne accès a l'objet http qui permet de creer le serveur
+const app = express();
 
 const normalizePort = val => {
     const port = parseInt(val, 10);
@@ -12,36 +13,27 @@ const normalizePort = val => {
     }
     return false;
 };
-const port = normalizePort(process.env.PORT || '3000'); // configuration du serveur pour qu'il écoute soit la variable d'environnement du port soit le port 3000
-app.set('port', port);
 
-const errorHandler = error => {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges.');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use.');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-};
-
-const server = http.createServer(app); //constante server avec http.createServer qui prend l'argument app
-
-server.on('error', errorHandler);
-server.on('listening', () => {
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    console.log('Listening on ' + bind);
+const port = normalizePort(process.env.BACK_END_SERVER_PORT || '3000'); // configuration du serveur pour qu'il écoute soit la variable d'environnement du port soit le port 3000
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
 });
 
-server.listen(port);
+app.use(express.json()); // Indique à l'application Express d'utiliser le middleware JSON (pour que nous puissions voir les corps de nos requêtes en JSON).
+
+app.get('/', (req, res) => { // Crée une route GET et envoie une réponse initiale.
+    res.status(200).json({message: "Hello from my-express-app!"});
+});
+
+const userRouter = require('./routes/user.routes');
+app.use('/users', userRouter);
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb+srv://melodieyeye:hMC6$T3ET3zn@bddproject.uiz3c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
