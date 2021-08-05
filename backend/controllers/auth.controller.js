94cb856e226537897f8a660c119f44e486bd4889
melodieyeye <yeyemelodie@outlook.fr>
2021-08-05 14:38:38 +0200

    edit page

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -10,7 +10,7 @@ exports.signUpRequest = async (req, res) => {
     const foundUser = await UserModel.find({name});
 
     // si aucun utilisateur n'est trouvé, nous pouvons ajouter cet utilisateur à la base de données.
-    if(!foundUser || foundUser.length == 0) {
+    if (!foundUser || foundUser.length == 0) {
         bcrypt.hash(req.body.password, 10)
             .then(hash => {
                 const user = new user({
@@ -18,10 +18,11 @@ exports.signUpRequest = async (req, res) => {
                     password: hash
                 });
                 user.save()
-                    .then(() => res.status(201).json({ message: 'Utilisateur crée !'}))
-                    .catch(error => res.status(400).json({ error }));
+                    .then(() => res.status(201).json({message: 'Utilisateur crée !'}))
+                    .catch(error => res.status(400).json({error}));
             })
-            .catch(error => res.status(500).json({ error }));
+            .catch(error => res.status(500).json({error}));
+    }
 }
 
 exports.loginRequest = async (req, res) => {
