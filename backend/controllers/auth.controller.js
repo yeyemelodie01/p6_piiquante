debf3399b7ea6e4ee5c27e51161a1718fcfdef27
melodieyeye <yeyemelodie@outlook.fr>
2021-08-05 14:08:17 +0200

    edit page

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,27 @@
+const bcrypt = require('bcrypt');
+const user = require('../models/user.model');
+
 exports.signUpRequest = async (req, res) => {
-    res.status(200).json('coucou');
-    console.log('coucou')
+    // req.body est pour les demandes POST. Pensez au "corps du facteur".
+    // déstructure la valeur du nom du corps de la requête.
+    const {name} = req.body;
+
+    // vérifier si la base de données contient déjà ce nom.
+    const foundUser = await UserModel.find({name});
+
+    // si aucun utilisateur n'est trouvé, nous pouvons ajouter cet utilisateur à la base de données.
+    if(!foundUser || foundUser.length == 0) {
+        bcrypt.hash(req.body.password, 10)
+            .then(hash => {
+                const user = new user({
+                    email: req.body.email,
+                    password: hash
+                });
+                user.save()
+                    .then(() => res.status(201).json({ message: 'Utilisateur crée !'}))
+                    .catch(error => res.status(400).json({ error }));
+            })
+            .catch(error => res.status(500).json({ error }));
 }
 
 exports.loginRequest = async (req, res) => {
