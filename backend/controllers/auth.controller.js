13e784c216a4e5ce9f4658b7b714d1621e835561
melodieyeye <yeyemelodie@outlook.fr>
2021-08-06 17:01:22 +0200

    edit page

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,42 +1,50 @@
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const userModel = require('../models/user.model');
-const jwt = require('njwt');
+const bcrypt = require('bcrypt'); // importation du module bcrypt
+const saltRounds = 10; // controle le temps necessaire pour calculer un seul hash
+const userModel = require('../models/user.model'); // importation du fichier user.model
+const jwt = require('njwt'); // importation du module njwt
 
-exports.signUpRequest = async (req, res) => {
-    const {email, password} = req.body;
-    const foundUser = await userModel.find({email});
 
-    if(!foundUser || foundUser.length === 0) {
-        const salt = bcrypt.genSaltSync(saltRounds);
-        const hash = bcrypt.hashSync(password, salt);
-        const user = new userModel({
-            email: email,
-            password : hash
+exports.signUpRequest = async (req, res) => { // export de la fonction signUpRequest avec pour valeur async parametre request et response
+    const {email, password} = req.body; //constantes variables email et password extraites de request.body
+    const foundUser = await userModel.find({email}); //constante foundUser avec pour valeur le résultat de userModel.find pour l'email
+
+    if(!foundUser || foundUser.length === 0) { // si foundUser est faux ou si foundUser.length est strictement égale a 0
+        const salt = bcrypt.genSaltSync(saltRounds); //constant salt qui contient le salt généré par bcrypt.genSaltSync
+        const hash = bcrypt.hashSync(password, salt); // permet de masquer le mot de passe
+        const user = new userModel({ // constante user avec pour valeur un nouvel objet de type userModel
+            email: email, // email qui prend la valeur de l'email
+            password : hash // le mot de passe qui sera hasher
         });
-        const response = await user.save();
-        res.status(201).json(response);
-    } else {
-        res.status(409).json({message: "User already exists!"});
+        const response = await user.save(); // constante response qui aura pour valeur le resultat de user.save
+        res.status(201).json(response); // reponse avec le statut 201 qui encode en json response
+    } else { //sinon
+        res.status(409).json({message: "User already exists!"}); // reponse avec le statut 409 qui encode en json le message User already exists
     }
 }
 
-exports.loginRequest = async (req, res) => {
-    userModel.findOne({email: req.body.email})
-        .then(user =>{
-            if(!user){
-                return res.status(409).json({message: "User not found"});
-            }else {
-                bcrypt.compare(req.body.password, user.password)
-                    .then(userValid =>{
-                        if (!userValid){
-                            return res.status(409).json({message: "incorrect password"});
-                        }else {
-                            res.status(201).json({
-                                userId: user._id,
-                            })
-                        }
-                    })
+exports.loginRequest = async (req, res) => { // export de la fonction loginRequest avec pour valeur asynchrone qui a pour parametre req et res
+    const {email, password} = req.body; //constantes variables email et password extraites de request.body
+    const foundUser = await userModel.findOne({email}); //constante foundUser avec pour valeur le résultat de userModel.findOne pour l'email
+
+    if(!foundUser || foundUser.length === 0) { // si foundUser est faux ou si foundUser.length est strictement égale a zero
+        res.status(409).json({message: "User not found"}); // response avec le status 409 qui encode le message User not found
+    }
+
+    if (foundUser) { // si foundUser
+        let checkPassword = bcrypt.compareSync(password, foundUser.password); // création de la variable checkPassword qui compare le mot de passe avec celui de foundUser.password
+        if (checkPassword === true) { // si checkPassword est strictement vrai
+            /// TOKEN BLABLA
+            const claims = { iss: 'fun-with-jwts', sub: foundUser._id } // constante claims qui a pour valeur un objet avec l'URL du site et le ID de l'utilisateur dans notre base de donnée
+            const token = jwt.create(claims, op-secret-phrase') // constante token qui crée le token
+            token.setExpiration(new Date().getTime() + 60*1000) //expiration du token
+
+            const userLoginInformation = { // constante userLoginInformation
+                userId: foundUser._id, // userId qui prend la valeur de foundUser._id
+                token: token.compact(), //token qui prend la valeur de token.compact
             }
-        })
+            res.status(302).json(userLoginInformation);// réponse avec le status 302 qui encode l'objet userLoginInformation
+        } else {
+            res.status(409).json({message: "User not found"}); // réponse avec le status 409 qui encode le message User not found
+        }
+    }
 }
