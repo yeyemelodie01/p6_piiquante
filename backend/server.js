25b1075135d1b950578607bc7eb11e4f7820e9c0
melodieyeye <yeyemelodie@outlook.fr>
2021-09-23 11:45:15 +0200

    edit page

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();//
+require('dotenv').config();// charge les variables d'environnement du fichier .env
 const express = require("express");// importation du module express
 const path = require('path');// importation du module path
 const helmet = require("helmet"); // importation du module helmet
@@ -6,46 +6,48 @@ const mongoose = require("mongoose");// importation du module mongoose
 const rateLimit = require("express-rate-limit"); //importation du module express rate limit
 
 const limiter = rateLimit({ // constante limiter qui a pour valeur rateLimit
-    windowMs : 15 * 60 * 1000,//
-    max : 250,
+    windowMs : 15 * 60 * 1000,// 15 minutes
+    max : 250, // 250 essaies
 });
 
-const app = express();
+const app = express(); // utilisation du module express
 
-app.use(limiter);
+app.use(limiter); // application de la constante limiter
 
 app.use(helmet());
 
 const normalizePort = val => {
     const port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        return val;
+    if (isNaN(port)) {// si "port" n'est pas un nombre
+        return val; // renvoie l'argument val
     }
-    if (port >= 0) {
-        return port;
+    if (port >= 0) { // si "port" plus grand ou égale à 0
+        return port;// renvoie "port"
     }
-    return false;
+    return false;// renvoie faux si port plus petit que 0
 };
 
 const port = normalizePort(process.env.BACK_END_SERVER_PORT || '3000'); // configuration du serveur pour qu'il écoute soit la variable d'environnement du port soit le port 3000
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
+
 app.use(express.json()); // Indique à l'application Express d'utiliser le middleware JSON (pour que nous puissions voir les corps de nos requêtes en JSON).
 
-app.get('/', (req, res) => { // Crée une route GET et envoie une réponse initiale.
+app.get('/', (req, res) => { // Crée une route GET qui envoie une réponse initiale.
     res.status(200).json({message: "Hello from my-express-app!"});
 });
 
 
-//MIDDLEWARE
+//MIDDLEWARE --- CORS ---
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // permet d'acceder a l'API depuis n'importe quelle origine
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');// ajoute les headers mentionnés aux requêtes envoyées vers l'API
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');// envoie des requêtes avec les méthodes mentionnées
     next();
 });
+
 
 mongoose.connect('mongodb+srv://'+process.env.DATABASE_USER+':'+process.env.DATABASE_PASSWORD+'@'+process.env.DATABASE_NAME,
     {
